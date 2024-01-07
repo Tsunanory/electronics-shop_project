6c5e534228bdd5914b90d8ae64c9901a47631ca5
Ivan Skliaruk <cunanori@gmail.com>
2024-01-07 12:10:40 +0100

    1 and 2 hw done

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+import sys
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -6,25 +9,40 @@ class Item:
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
-
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        """
-        pass
+        """Создание экземпляра класса item."""
+        self.__name = name
+        self.price = price
+        self.quantity = quantity
+        #self.all.append(self) #Для homework-2 этa команда перенесена в функцию instantiate_from_csv
 
     def calculate_total_price(self) -> float:
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
-
-        :return: Общая стоимость товара.
-        """
-        pass
+        """Рассчитывает общую стоимость конкретного товара в магазине."""
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
-        """
-        Применяет установленную скидку для конкретного товара.
-        """
-        pass
+        """Применяет установленную скидку для конкретного товара."""
+        self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, path):
+        with open(path, 'r',
+                  encoding='cp1251') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                Item.all.append(Item(row['name'], int(row['price']), int(row['quantity'])))
+
+    @staticmethod
+    def string_to_number(string):
+        if '.' in string:
+            string = string[:string.index('.')]
+        return int(string)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        if len(new_name) > 10:
+            new_name = new_name[:10]
+        self.__name = new_name
